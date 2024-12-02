97f14fed2f72f983b311ff017427c1c2c955cabb
jw118g <jw118g@gmail.com>
2024-12-02 09:18:14 +0900

    헤더수정

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
-const lenis = new Lenis();
-
-lenis.on('scroll', ScrollTrigger.update);
-
-gsap.ticker.add((time) => {
-  lenis.raf(time * 600); // Convert time from seconds to milliseconds
-});
-
-gsap.ticker.lagSmoothing(0);
